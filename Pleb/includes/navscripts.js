7322dce583190874ba536d1f0366d31d7ffb9d1e
bowsse <jaakko.poyhonen@gmail.com>
2016-11-21 10:01:19 +0000

    vittu

---
+++ Pleb/includes/navscripts.js
@@ -0,0 +1,7 @@
+function openLogin() {
+            document.getElementById('popupbox').style.visibility="visible";
+        }
+                
+function closeLogin() {
+    document.getElementById('popupbox').style.visibility="hidden"; 
+}
---
+++ Pleb/index.php
@@ -0,0 +1,136 @@
+<head>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="styles/style.css" type="text/css" rel=stylesheet>
+	<script src="scripts/indexScript.js"></script> <!-- index.php soittolistan nappiscriptit -->
+	<script src="scripts/YTPlayerScript.js"></script> <!-- index.php YouTube-soittimen scriptit -->
+	<script language="JavaScript" type="text/javascript">
+	function login(showhide){
+	if(showhide == "show"){
+	    document.getElementById('popupbox').style.visibility="visible";
+		}else if(showhide == "hide"){
+		    document.getElementById('popupbox').style.visibility="hidden"; 
+		}
+	}
+	</script>
+	</head>				      
+<body>
+<!--<div id="popupbox"> 
+<form name="loginform" action="<?php echo htmlentities($_SERVER['SCRIPT_NAME']);?>" method="post">
+<center>Username:</center>
+<center><input id="login_input_username" type="text" name="user_name" required /></center>
+<center>Password:</center>
+<center><input id="login_input_password" type="password" name="user_password" required /></center>
+<center><input type="submit"  name="login" value="Log in"</center>
+</form>
+<br />
+<center><a href="javascript:login('hide');">close</a></center> 
+</div> -->
+<?php include_once("includes/nav.php");
+ ?> <!-- header ja navigointilinkit -->
+
+<main>
+	<!-- sivun vasen puoli -->
+	<section>
+		<!-- YouTube-soittimen div -->
+			<div class="wrapper">
+				<div id="player"></div>
+			</div>
+		<!-- YouTube-soittimen napit -->
+			<article id=playerButtons>
+				<button class="playerButton" id="play" onclick="playVideo()">Play</button><button class="playerButton" id="skip" onclick="skipVideo()">Skip</button><button class="playerButton" id="mute" onclick="muteVideo()">Mute</button><input id="volume" type=range min="0" max="100" value=100 oninput="changeVolume(this.value)">
+			</article>
+		<!-- Uuden kappaleen lisäyslomake -->
+			<article>
+				<ul class="tab">
+				  <li><a href="javascript:void(0)" class="tablinks" onclick="openTab(event, 'Lisää')" id="defaultOpen">Lisää kappale</a>
+					</li>
+				  <li><a href="javascript:void(0)" class="tablinks" onclick="openTab(event, 'chat')">chat</a></li>
+				</ul>
+
+				<div id="Lisää" class="tabcontent">
+					<h1>Lisää kappale</h1>
+					<input type="text" id="videoUrl" name="videoUrl" placeholder="https://www.youtube.com/watch?v=1yhTaFQJukx" onInput="getData(this.value)"><button onclick="addVideo()">>></button>
+					<br><p>Title: <span id=videoTitle name=videoTitle></span></p>
+					<p>Channel: <span id=channelTitle name=channelTitle></span></p>
+				</div>
+
+				<div id="chat" class="tabcontent">
+				  <div id="mastercontainer">
+				<h1>HUUTOLOOTA >:DDD</h1>
+
+				<p id='navbar'>Select Role: <a href='index.php?user_id=2'><span style='background-color: #ffc'>Admin</span></a>
+				</p>    <div id="container"></div>
+
+					<script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+				    <script src="https://unpkg.com/react@15.3.2/dist/react.js"></script>
+				    <script src="https://unpkg.com/react-dom@15.3.2/dist/react-dom.js"></script>
+				    <script src="https://unpkg.com/babel-core@5.8.38/browser.min.js"></script>
+				    <script type="text/babel" src="js/react-chat.js"></script>
+
+			</div>
+				</div>
+			</article>
+	</section>
+	<!-- sivun oikea puoli -->
+	<!-- soittolista -->
+		<aside id="playlist">
+			
+			<table>
+				<?php printTable(); ?>
+			</table>
+		
+		</aside>
+	<button id="playlistButton" onclick="closeList()">Close</button> <!-- soittolistan nappi -->
+
+		<!-- chat -->
+		<!--<aside id="chat">
+			<div id="mastercontainer">
+				<h1>HUUTOLOOTA >:DDD</h1>
+
+				<p id='navbar'>Select Role: <a href='index.php?user_id=2'><span style='background-color: #ffc'>Admin</span></a>
+				</p>    <div id="container"></div>
+
+					<script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+				    <script src="https://unpkg.com/react@15.3.2/dist/react.js"></script>
+				    <script src="https://unpkg.com/react-dom@15.3.2/dist/react-dom.js"></script>
+				    <script src="https://unpkg.com/babel-core@5.8.38/browser.min.js"></script>
+				    <script type="text/babel" src="js/react-chat.js"></script>
+
+			</div>
+		</aside>
+		<button id="chatButton" onclick="closeChat()">Close</button>--> <!-- chatin nappi -->
+
+		
+</main>
+
+<?php include_once("includes/footer.php"); ?> <!-- footer -->
+<script>
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+</script>
+</body>
+
+<?php
+//soittolistan tulostusfunktio
+function printTable(){
+	$db = new SQLite3('/var/Databases/testdb.db'); //tietokanta
+	
+	$results = $db->prepare('SELECT * FROM Video'); //... WHERE id = :id' //tietokantahaun alustus
+	//$results->bindValue(':id',$id);
+	$result = $results->execute(); //tietokantahaku
+	
+	//soittolistataulukon tulostus
+	while($row = $result->fetchArray()) {
+		echo	"<tr><td><img class='thumbnail' src='https://i.ytimg.com/vi/".$row[3]."/mqdefault.jpg' onclick=changeVideo('".$row[3]."')></td>
+				<td>
+					<ul class='test'>
+						<li><strong>".$row[1]."</strong></li>
+						<li>".$row[2]."</li>
+						<li><i>".$row[4]."</i></li>
+					</ul>
+				</td>
+				</tr>";
+	}
+}
+?>
